374816efe7602c22af814ac33537a212d155c4f2
ra-gose-dev <amorbarroa@gmail.com>
2020-10-03 21:47:01 +0800

    added sharpdevelop web api proj

---
+++ SharpDevelopWebApi/Models/SDWebApiDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SharpDevelopWebApi.Models
+{
+    public class SDWebApiDbContext : DbContext
+    {
+        public SDWebApiDbContext() : base("DefaultConn") // name_of_dbconnection_string
+        {
+        }
+
+        // Map model classes to database tables
+        public DbSet<UserAccount> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+    }
+
+
+}
+
