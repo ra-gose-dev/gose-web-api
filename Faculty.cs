3c5b76ca35a4c0cbd3df061ccec227028355355c
ra-gose-dev <amorbarroa@gmail.com>
2020-10-19 21:54:33 +0800

    added classed

---
+++ Faculty.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace SharpDevelopWebApi.Models
+{
+	
+	public class Faculty : Person
+	{
+		public string SSSNumber { get; set; }
+		public string Supervisor { get; set; }
+	}
+}
---
+++ StudentGrade.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace SharpDevelopWebApi.Models
+{
+	
+	public class StudentGrade
+	{
+		public int Id { get; set; }
+		public int StudentId { get; set; }
+		public int SubjectId { get; set; }
+		public double P1Grade { get; set; }
+		public double P2Grade { get; set; }
+		public double P3Grade { get; set; }
+	}
+}
